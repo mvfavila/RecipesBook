a5f2631af98a43f25ffae12ca1883dc78adcee98
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-06-08 23:38:03 +0200

    Adding new items to ingredients list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
   selector: "app-shopping-edit",
@@ -6,7 +14,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild("nameInput", { static: true }) nameInputRef: ElementRef;
+  @ViewChild("amountInput", { static: true }) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  onAddItem() {
+    const ingredientName = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
+    this.ingredientAdded.emit(new Ingredient(ingredientName, ingredientAmount));
+  }
 }
