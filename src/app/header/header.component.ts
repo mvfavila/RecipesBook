16e3154864f3b53fbaba3ef14f2fb1483bb1c188
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-06-08 22:10:30 +0200

    Basic navigation added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
   collapsed = true;
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(feature: string) {
+    switch (feature) {
+      case "recipe":
+      case "shopping-list":
+        this.featureSelected.emit(feature);
+        break;
+      default:
+        throw new Error("Invalid feature");
+        break;
+    }
+  }
 }
