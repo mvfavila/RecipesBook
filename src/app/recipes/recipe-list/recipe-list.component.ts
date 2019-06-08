54ab1905cc878d7295f872a097967e85cd03993c
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-06-08 23:13:34 +0200

    Recipe Detail being loaded

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -12,10 +12,20 @@ export class RecipeListComponent implements OnInit {
       "First recipe",
       "Description",
       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"
+    ),
+    new Recipe(
+      "Crabs",
+      "Best food ever",
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/Caranguejo_de_Sergipe.jpg"
     )
   ];
+  @Output() recipeItemSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeItemSelected.emit(recipe);
+  }
 }
