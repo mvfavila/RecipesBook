cc59b3cd227f65e638cda805fdd248c67253e1dd
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-06-11 18:53:56 +0200

    Passing selected recipe through recipe service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -10,11 +10,11 @@ import { RecipeService } from "./recipe.service";
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
-
-  onRecipeItemSelected(recipe: Recipe) {
-    this.selectedRecipe = recipe;
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => (this.selectedRecipe = recipe)
+    );
   }
 }
