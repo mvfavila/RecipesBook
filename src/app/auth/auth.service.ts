8899a53588a3b3be8f3c82b8133038b91326dca2
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-06-20 23:07:09 +0200

    Added AutoLogin and AutoLogout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,6 +22,7 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
+  private tokenExpirationTimer: any;
   private readonly returnSecureToken = true;
   private API_KEY = "AIzaSyCY3Xn4FIlR0Pn2pufF14nktxhErT4uJdA";
   private signupBaseUrl = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
@@ -75,7 +76,46 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
+    localStorage.removeItem("userData");
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
     this.router.navigate(["/auth"]);
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -119,5 +159,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 }
