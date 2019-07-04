4e7be432ac5b8671e12c0736d79e51bc2e86b16b
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-07-04 11:08:55 +0200

    Added preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" }
 ];
 
 const appChildRoutes: Routes = [
-  {path: "auth", loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  {path: "recipes", loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
-  {path: "shopping-list", loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)}
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
+  },
+  {
+    path: "recipes",
+    loadChildren: () =>
+      import("./recipes/recipes.module").then(m => m.RecipesModule)
+  },
+  {
+    path: "shopping-list",
+    loadChildren: () =>
+      import("./shopping-list/shopping-list.module").then(
+        m => m.ShoppingListModule
+      )
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes), RouterModule.forChild(appChildRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forChild(appChildRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { HeaderComponent } from "./header/header.component";
-import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { BasicHighlightDirective } from "./shared/basic-highlight.directive";
 import { UnlessDirective } from "./shared/unless.directive";
-import { AuthModule } from "./auth/auth.module";
 import { SharedModule } from "./shared/shared.module";
-import { RecipeService } from './recipes/recipe.service';
+import { RecipeService } from "./recipes/recipe.service";
 
 @NgModule({
   declarations: [
@@ -20,14 +18,7 @@ import { RecipeService } from './recipes/recipe.service';
     BasicHighlightDirective,
     UnlessDirective
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ShoppingListModule,
-    AuthModule,
-    SharedModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
   providers: [
     RecipeService,
     {
