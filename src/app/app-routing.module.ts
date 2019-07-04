af601e9a0025acd637a2cb37805614b9145272e5
Avila, Marcos <Marcos.Avila@Monster.com>
2019-07-04 10:48:58 +0200

    Added lazy loading for Auth and ShoppingList modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const appRoutes: Routes = [
 ];
 
 const appChildRoutes: Routes = [
-  {path: "recipes", loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)}
+  {path: "auth", loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: "recipes", loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+  {path: "shopping-list", loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)}
 ];
 
 @NgModule({
